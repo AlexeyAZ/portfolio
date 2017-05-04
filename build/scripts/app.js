6b29e8f5a62dffffd61be833c9e1c4086729d70d
Alexey <teos.nl@gmail.com>
2017-05-04 18:03:13 +0300

    route

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,15 +1,44 @@
 $(function () {
 
-	let header = Vue.component("site-header", {
-		template: "<header class='header'>{{ message }}</header>",
-		data: function () {
-			message: "hello";
-		}
-	});
+    Vue.component("site-nav", {
+        props: ["list"],
+        template: '#site-nav'
+    });
 
-	let app = new Vue({
-		el: "#app",
-		data: {}
-	});
+    Vue.component("page-main", {
+        props: ["list"],
+        template: '#page-main'
+    });
+
+    Vue.component("page-gallery", {
+        props: ["list"],
+        template: '#page-gallery'
+    });
+
+    let app = new Vue({
+        el: "#app",
+        data: {
+            parentMessage: "",
+            location: {
+                main: true,
+                gallery: false
+            },
+            items: [{
+                name: "Main",
+                href: "http://",
+                location: "main"
+            }, {
+                name: "Gallery",
+                href: "http://",
+                location: "gallery"
+            }]
+        },
+
+        methods: {
+            setLocation: function (item) {
+                this.location[item.location] = true;
+            }
+        }
+    });
 });
 //# sourceMappingURL=app.js.map
