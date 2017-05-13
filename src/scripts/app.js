58f79065c90610c0c95a68cd75294f982e02a52d
Alexey <teos.nl@gmail.com>
2017-05-13 16:35:05 +0300

    animations

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,14 +1,14 @@
 $(function() {
 
     Vue.component("site-nav", {
-        props: ["list", "link"],
+        props: ["list", "navlocation"],
         template: '#site-nav',
         methods: {
 
-            setLocation: function() {
-                this.$emit("link_click", this.link.location);
-            }
-        }
+            setLocation: function(item) {
+                this.$emit("link_click", item.location);
+            },
+        },
     });
 
     Vue.component("page-main", {
@@ -19,12 +19,50 @@ $(function() {
     Vue.component("page-gallery", {
         props: ["list"],
         template: '#page-gallery',
+        data: function() {
+            return {
+                galleryListHeight: 300,
+                listAr: this.list
+            }
+        },
+        mounted: function() {
+            // this.$nextTick(function() {
+            //     window.addEventListener("resize", this.setGalleryListHeight);
+            //     this.setGalleryListHeight();
+            // });
+            
+            window.addEventListener("resize", this.setGalleryListHeight);
+
+            this.$http.get(
+                'json/gallery.json'
+            ).then(
+                function(response) {
+                    this.listAr = response.data;
+                    this.$nextTick(function() {
+                        this.setGalleryListHeight();
+                    });
+                }
+            )
+        },
+        methods: {
+
+            setGalleryListHeight: function() {
+                //this.galleryListHeight = (this.$el.clientWidth / 3) / 1.5
+                this.galleryListHeight = document.querySelector(".gallery__list-item").clientWidth / 1.5;
+            }
+        },
+        watch: {
+
+            list: function(val, oldval) {
+                this.setGalleryListHeight();
+            }
+        }
     });
 
     var app = new Vue({
         el: "#app",
         data: {
-            appLocation: "main",
+            appLocation: "gallery",
             contentMove: true,
             links: [
                 {
@@ -36,18 +74,26 @@ $(function() {
                     name: "Gallery",
                     href: "http://",
                     location: "gallery"
-                }
+                },
             ],
-            sites: [
-                {
-                    name: "site1",
-                    href: "http://",
-                    imgsrc: "img/img.jpg"
-                }
-            ]
+            sites: []
+        },
+
+        mounted: function() {
+            // this.$http.get(
+            //     'json/gallery.json'
+            // ).then(
+            //     function(response) {
+            //         this.createArray(response.data);
+            //     }
+            // )
         },
 
         methods: {
+
+            createArray: function(arr) {
+                this.sites = arr;
+            },
 
             renderLocation: function(location) {
                 console.log(location)
