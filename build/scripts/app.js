f0cdb8981715957b84f99c187a4c045e5fb95584
Alexey <teos.nl@gmail.com>
2017-05-20 11:54:49 +0300

    smooth load image

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,190 +1,191 @@
-$(function () {
 
-    Vue.component("site-nav", {
-        props: ["list", "navlocation"],
-        template: '#site-nav',
-        methods: {
+Vue.component("site-nav", {
+    props: ["list", "navlocation"],
+    template: '#site-nav',
+    methods: {
 
-            setLocation: function (item) {
-                console.log(item);
-                this.$emit("link_click", item.location);
-            }
+        setLocation: function (item) {
+            console.log(item);
+            this.$emit("link_click", item.location);
         }
-    });
+    }
+});
 
-    Vue.component("page-main", {
-        props: ["list"],
-        template: '#page-main'
-    });
+Vue.component("page-main", {
+    props: ["list"],
+    template: '#page-main'
+});
 
-    Vue.component("page-gallery", {
-        props: ["list"],
-        template: '#page-gallery',
-        data: function () {
+Vue.component("page-gallery", {
+    props: ["list"],
+    template: '#page-gallery',
+    data: function () {
 
-            return {
-                galleryListHeight: 300,
-                listAr: this.list,
-                galleryColumn: 3,
-                galleryScrollTop: 0,
-                galleryHeight: null
-            };
-        },
+        return {
+            galleryListHeight: 300,
+            listAr: this.list,
+            galleryColumn: 1,
+            galleryScrollTop: 0,
+            galleryHeight: null,
+            galleryRowShow: 1
+        };
+    },
 
-        mounted: function () {
-            var self = this;
+    mounted: function () {
+        var self = this;
 
-            window.addEventListener("resize", function () {
-                self.setGalleryListHeight();
-            });
+        window.addEventListener("resize", function () {
+            self.setGalleryListHeight();
+        });
 
-            this.$http.get('json/gallery.json').then(function (response) {
-                var data = response.data;
-                var item = data[0];
-                var arr = [];
+        this.$http.get('json/gallery.json').then(function (response) {
+            var data = response.data;
+            var item = data[0];
+            var arr = [];
 
-                for (var i = 0; i < 20; i++) {
+            for (var i = 0; i < 100; i++) {
 
-                    arr.push({
-                        "name": "site1",
-                        "href": "http://",
-                        "imgsrc": "http://web-dev.pw/images/i_shop.jpg",
-                        "gitlink": "http://",
-                        "imgload": "http://web-dev.pw/images/i_shop.jpg",
-                        "visible": "false",
-                        "top": "0"
-                    });
-                }
-                // for (var i = 0; i < 10; i++) {
-
-                // 	for(var j = 0; j < data.length; j++) {
-                // 		arr.push(data[j]);
-                // 	}
-                // }
-
-                // arr.splice(arr.length - 1, 1);
-
-                this.listAr = arr;
-
-                this.$nextTick(function () {
-                    this.setGalleryListHeight();
-                    this.setGalleryItemHeightAttr();
-                    this.galleryHeight = this.$el.querySelector(".gallery__list").scrollHeight;
+                arr.push({
+                    "name": "site1",
+                    "href": "http://",
+                    "imgsrc": "http://web-dev.pw/images/i_shop.jpg",
+                    "gitlink": "http://",
+                    "imgload": "false",
+                    "visible": "false",
+                    "top": "0"
                 });
-            });
-        },
-        methods: {
-
-            setTop: function (item) {
-                if (item.top - this.galleryScrollTop < document.documentElement.clientHeight && item.visible === "false") {
-                    return item.imgload;
-                } else {
-                    return false;
-                }
-            },
-
-            setGalleryItemHeightAttr: function () {
-                var coef = 1;
-                var height = 1;
-
-                for (var i = 0; i < this.listAr.length; i++) {
-
-                    if (i % this.galleryColumn === 0) {
-                        coef++;
-                    }
-
-                    height = this.galleryListHeight * (coef - 1);
-                    this.listAr[i].top = height;
-                    console.log(height);
-                }
-            },
-
-            getDisplayHeight: function () {
-                //this.displayHeight = 
-            },
-
-            setGalleryListHeight: function () {
-                //this.galleryListHeight = (this.$el.clientWidth / 3) / 1.5
-                this.galleryListHeight = document.querySelector(".gallery__list-item").clientWidth / 1.5;
-            },
-
-            loadImg: function () {
-                var self = this;
-
-                var img = new Image();
-                img.onload = function () {
-                    console.log("image is loaded");
-                    console.log(self.listAr[2]);
-                    self.listAr[2].imgload = "http://www.planwallpaper.com/static/images/Nikon-D810-Image-Sample-6.jpg";
-                };
-                img.src = "http://www.planwallpaper.com/static/images/Nikon-D810-Image-Sample-6.jpg";
-            },
-
-            checkVisible: function (e) {
-
-                this.galleryScrollTop = e.target.scrollTop;
-                console.log(this.galleryScrollTop);
-                console.log("scroll-height: " + this.$el.querySelector(".gallery__list").scrollHeight);
-                this.galleryHeight = this.$el.querySelector(".gallery__list").scrollHeight;
-                for (var i = 0; i < this.listAr.length; i++) {}
             }
-        },
-        watch: {
 
-            list: function (val, oldval) {
+            this.listAr = data;
+
+            this.$nextTick(function () {
                 this.setGalleryListHeight();
-            }
-        }
-    });
+                this.setGalleryItemHeightAttr();
+                this.galleryHeight = this.$el.querySelector(".gallery__list").scrollHeight;
+                this.checkVisible();
+            });
+        });
+    },
+    methods: {
 
-    var app = new Vue({
-        el: "#app",
-        data: {
-            appLocation: "gallery",
-            contentMove: true,
-            links: [{
-                name: "Main",
-                href: "http://",
-                location: "main"
-            }, {
-                name: "Gallery",
-                href: "http://",
-                location: "gallery"
-            }],
-            sites: []
+        setGalleryItemHeightAttr: function () {
+            var coef = 1;
+            var height = 1;
+
+            for (var i = 0; i < this.listAr.length; i++) {
+
+                if (i % this.galleryColumn === 0) {
+                    coef++;
+                }
+
+                height = this.galleryListHeight * (coef - 1);
+                this.listAr[i].top = height;
+                console.log(height);
+            }
         },
 
-        mounted: function () {
-            // this.$http.get(
-            //     'json/gallery.json'
-            // ).then(
-            //     function(response) {
-            //         this.createArray(response.data);
-            //     }
-            // )
+        setGalleryListHeight: function () {
+            this.galleryListHeight = document.querySelector(".gallery__list-item").clientWidth / 1.5;
         },
 
-        methods: {
+        checkVisible: function (e) {
 
-            createArray: function (arr) {
-                this.sites = arr;
-            },
+            var self = this;
+            var rows = 0;
+            var rowTop = 0;
+            var coeff = 0;
 
-            renderLocation: function (location) {
-                console.log(location);
-                this.appLocation = location;
-            },
+            if (e) {
+                this.galleryScrollTop = e.target.scrollTop;
+            } else {
+                this.galleryScrollTop = this.$el.querySelector(".gallery__list").scrollTop;
+            }
 
-            headerMouseover: function () {
-                this.contentMove = !this.contentMove;
-                console.log(this.contentMove);
-            },
+            for (let i = 0; i < this.listAr.length; i++) {
 
-            headerMouseout: function () {
-                this.contentMove = !this.contentMove;
-                console.log(this.contentMove);
+                if (this.listAr[i].top !== rowTop) {
+                    rowTop = this.listAr[i].top;
+                    rows++;
+                }
+            }
+
+            for (let i = 1; i <= rows; i++) {
+
+                if (this.galleryListHeight * i - this.galleryScrollTop < document.documentElement.clientHeight) {
+
+                    for (let j = 0; j < this.galleryColumn; j++) {
+                        let number = j + coeff;
+
+                        if (typeof this.listAr[number] !== "undefined" && this.listAr[number].visible === "false") {
+
+                            let img = new Image();
+                            img.onload = function () {
+                                self.listAr[number].imgload = self.listAr[number].imgsrc;
+                                self.listAr[number].visible = "true";
+                            };
+                            img.src = self.listAr[number].imgsrc;
+                        }
+                    }
+                    coeff += this.galleryColumn;
+                }
             }
         }
-    });
+    },
+    watch: {
+
+        list: function (val, oldval) {
+            this.setGalleryListHeight();
+        }
+    }
+});
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        appLocation: "gallery",
+        contentMove: true,
+        links: [{
+            name: "Main",
+            href: "http://",
+            location: "main"
+        }, {
+            name: "Gallery",
+            href: "http://",
+            location: "gallery"
+        }],
+        sites: []
+    },
+
+    mounted: function () {
+        // this.$http.get(
+        //     'json/gallery.json'
+        // ).then(
+        //     function(response) {
+        //         this.createArray(response.data);
+        //     }
+        // )
+    },
+
+    methods: {
+
+        createArray: function (arr) {
+            this.sites = arr;
+        },
+
+        renderLocation: function (location) {
+            console.log(location);
+            this.appLocation = location;
+        },
+
+        headerMouseover: function () {
+            this.contentMove = !this.contentMove;
+            console.log(this.contentMove);
+        },
+
+        headerMouseout: function () {
+            this.contentMove = !this.contentMove;
+            console.log(this.contentMove);
+        }
+    }
 });
 //# sourceMappingURL=app.js.map
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,201 +1,199 @@
-$(function() {
 
-    Vue.component("site-nav", {
-        props: ["list", "navlocation"],
-        template: '#site-nav',
-        methods: {
+Vue.component("site-nav", {
+    props: ["list", "navlocation"],
+    template: '#site-nav',
+    methods: {
 
-            setLocation: function(item) {
-				console.log(item);
-                this.$emit("link_click", item.location);
-            },
+        setLocation: function(item) {
+            console.log(item);
+            this.$emit("link_click", item.location);
         },
-    });
+    },
+});
 
-    Vue.component("page-main", {
-        props: ["list"],
-        template: '#page-main',
-    });
+Vue.component("page-main", {
+    props: ["list"],
+    template: '#page-main',
+});
 
-    Vue.component("page-gallery", {
-        props: ["list"],
-        template: '#page-gallery',
-        data: function() {
+Vue.component("page-gallery", {
+    props: ["list"],
+    template: '#page-gallery',
+    data: function() {
 
-            return {
-                galleryListHeight: 300,
-                listAr: this.list,
-				galleryColumn: 3,
-				galleryScrollTop: 0,
-				galleryHeight: null
-            }
-        },
-		
-        mounted: function() {
-            var self = this;
+        return {
+            galleryListHeight: 300,
+            listAr: this.list,
+            galleryColumn: 1,
+            galleryScrollTop: 0,
+            galleryHeight: null,
+            galleryRowShow: 1
+        }
+    },
+    
+    mounted: function() {
+        var self = this;
 
-            window.addEventListener("resize", function() {
-                self.setGalleryListHeight();
-            });
+        window.addEventListener("resize", function() {
+            self.setGalleryListHeight();
+        });
 
-            this.$http.get(
-                'json/gallery.json'
-            ).then(
-                function(response) {
-					var data = response.data;
-					var item = data[0];
-					var arr = [];
+        this.$http.get(
+            'json/gallery.json'
+        ).then(
+            function(response) {
+                var data = response.data;
+                var item = data[0];
+                var arr = [];
 
-                    for(var i = 0; i < 20; i++) {
+                for(var i = 0; i < 100; i++) {
 
-                        arr.push({
-                            "name": "site1",
-                            "href": "http://",
-                            "imgsrc": "http://web-dev.pw/images/i_shop.jpg",
-                            "gitlink": "http://",
-                            "imgload": "http://web-dev.pw/images/i_shop.jpg",
-                            "visible": "false",
-							"top": "0"
-                        });
-                    }
-					// for (var i = 0; i < 10; i++) {
-
-					// 	for(var j = 0; j < data.length; j++) {
-					// 		arr.push(data[j]);
-					// 	}
-					// }
-
-					// arr.splice(arr.length - 1, 1);
-					
-                    this.listAr = arr;
-
-					
-                    this.$nextTick(function() {
-                        this.setGalleryListHeight();
-						this.setGalleryItemHeightAttr();
-						this.galleryHeight = this.$el.querySelector(".gallery__list").scrollHeight;
+                    arr.push({
+                        "name": "site1",
+                        "href": "http://",
+                        "imgsrc": "http://web-dev.pw/images/i_shop.jpg",
+                        "gitlink": "http://",
+                        "imgload": "false",
+                        "visible": "false",
+                        "top": "0"
                     });
                 }
-            )
+                
+                this.listAr = data;
+
+                
+                this.$nextTick(function() {
+                    this.setGalleryListHeight();
+                    this.setGalleryItemHeightAttr();
+                    this.galleryHeight = this.$el.querySelector(".gallery__list").scrollHeight;
+                    this.checkVisible();
+                });
+            }
+        )
+    },
+    methods: {
+
+        setGalleryItemHeightAttr: function() {
+            var coef = 1;
+            var height = 1;
+
+            for(var i = 0; i < this.listAr.length; i++) {
+
+                if (i % this.galleryColumn === 0) {
+                    coef++;
+                }
+
+                height = this.galleryListHeight * (coef - 1);
+                this.listAr[i].top = height;
+                console.log(height);
+            }
         },
-        methods: {
 
-			setTop: function(item) {
-				if(item.top - this.galleryScrollTop < document.documentElement.clientHeight && item.visible === "false") {
-					return item.imgload;
-				} else {
-					return false;
-				}
-			},
+        setGalleryListHeight: function() {
+            this.galleryListHeight = document.querySelector(".gallery__list-item").clientWidth / 1.5;
+        },
 
-			setGalleryItemHeightAttr: function() {
-				var coef = 1;
-				var height = 1;
+        checkVisible: function(e) {
+            
+            var self = this;
+            var rows = 0;
+            var rowTop = 0;
+            var coeff = 0;
 
-				for(var i = 0; i < this.listAr.length; i++) {
+            if(e) {
+                this.galleryScrollTop = e.target.scrollTop;
+            } else {
+                this.galleryScrollTop = this.$el.querySelector(".gallery__list").scrollTop;
+            }
 
-					if (i % this.galleryColumn === 0) {
-						coef++;
-					}
-
-					height = this.galleryListHeight * (coef - 1);
-					this.listAr[i].top = height;
-					console.log(height);
-				}
-			},
-
-            getDisplayHeight: function() {
-                //this.displayHeight = 
-            },
-
-            setGalleryListHeight: function() {
-                //this.galleryListHeight = (this.$el.clientWidth / 3) / 1.5
-                this.galleryListHeight = document.querySelector(".gallery__list-item").clientWidth / 1.5;
-            },
-
-			loadImg: function() {
-				var self = this;
-				
-				var img = new Image();
-				img.onload = function () {
-					console.log("image is loaded");
-					console.log(self.listAr[2])
-					self.listAr[2].imgload = "http://www.planwallpaper.com/static/images/Nikon-D810-Image-Sample-6.jpg";
-				}
-				img.src = "http://www.planwallpaper.com/static/images/Nikon-D810-Image-Sample-6.jpg";
-			},
-
-            checkVisible: function(e) {
-
-				this.galleryScrollTop = e.target.scrollTop;
-                console.log(this.galleryScrollTop);
-				console.log("scroll-height: " + this.$el.querySelector(".gallery__list").scrollHeight)
-				this.galleryHeight = this.$el.querySelector(".gallery__list").scrollHeight;
-                for (var i = 0; i < this.listAr.length; i++) {
-
+            for(let i = 0; i < this.listAr.length; i++) {
+                
+                if (this.listAr[i].top !== rowTop) {
+                    rowTop = this.listAr[i].top;
+                    rows++;
                 }
             }
-        },
-        watch: {
 
-            list: function(val, oldval) {
-                this.setGalleryListHeight();
+            for(let i = 1; i <= rows; i++) {
+                
+                if ((this.galleryListHeight * i - this.galleryScrollTop) < document.documentElement.clientHeight) {
+                    
+                    for (let j = 0; j < this.galleryColumn; j++) {
+                        let number = j + coeff;
+
+                        if (typeof this.listAr[number] !== "undefined" && this.listAr[number].visible === "false") {
+            
+                            let img = new Image();
+                            img.onload = function () {
+                                self.listAr[number].imgload = self.listAr[number].imgsrc;
+                                self.listAr[number].visible = "true";
+                            }
+                            img.src = self.listAr[number].imgsrc;
+                        }
+                    }
+                    coeff += this.galleryColumn;
+                }
             }
         }
-    });
+    },
+    watch: {
 
-    var app = new Vue({
-        el: "#app",
-        data: {
-            appLocation: "gallery",
-            contentMove: true,
-            links: [
-                {
-                    name: "Main",
-                    href: "http://",
-                    location: "main"
-                },
-                {
-                    name: "Gallery",
-                    href: "http://",
-                    location: "gallery"
-                },
-            ],
-            sites: []
-        },
-
-        mounted: function() {
-            // this.$http.get(
-            //     'json/gallery.json'
-            // ).then(
-            //     function(response) {
-            //         this.createArray(response.data);
-            //     }
-            // )
-        },
-
-        methods: {
-
-            createArray: function(arr) {
-                this.sites = arr;
-            },
-
-            renderLocation: function(location) {
-                console.log(location)
-                this.appLocation = location;
-            },
-
-            headerMouseover: function() {
-                this.contentMove = !this.contentMove;
-                console.log(this.contentMove)
-            },
-
-            headerMouseout: function() {
-                this.contentMove = !this.contentMove;
-                console.log(this.contentMove)
-            }
+        list: function(val, oldval) {
+            this.setGalleryListHeight();
         }
-    })
-
+    }
 });
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        appLocation: "gallery",
+        contentMove: true,
+        links: [
+            {
+                name: "Main",
+                href: "http://",
+                location: "main"
+            },
+            {
+                name: "Gallery",
+                href: "http://",
+                location: "gallery"
+            },
+        ],
+        sites: []
+    },
+
+    mounted: function() {
+        // this.$http.get(
+        //     'json/gallery.json'
+        // ).then(
+        //     function(response) {
+        //         this.createArray(response.data);
+        //     }
+        // )
+    },
+
+    methods: {
+
+        createArray: function(arr) {
+            this.sites = arr;
+        },
+
+        renderLocation: function(location) {
+            console.log(location)
+            this.appLocation = location;
+        },
+
+        headerMouseover: function() {
+            this.contentMove = !this.contentMove;
+            console.log(this.contentMove)
+        },
+
+        headerMouseout: function() {
+            this.contentMove = !this.contentMove;
+            console.log(this.contentMove)
+        }
+    }
+})
 
